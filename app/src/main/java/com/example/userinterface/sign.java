1ea1c510b486ff1c69a5b82100debca6e715ae92
omj <alswo030203@gmail.com>
2024-11-06 20:58:04 +0900

    회원가입 툴바 만드는중

--- app/src/main/java/com/example/userinterface/sign.java
+++ app/src/main/java/com/example/userinterface/sign.java
@@ -1,8 +1,11 @@
 package com.example.userinterface;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,13 +14,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.userinterface.databinding.ActivitySignBinding;
 
 public class sign extends AppCompatActivity {
-
+    private Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivitySignBinding binding = ActivitySignBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        back = binding.back;
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
