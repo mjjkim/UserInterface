919fa2b3a99dba529e7f0a114cdbcc19d990ef6d
omj <alswo030203@gmail.com>
2024-11-06 19:28:18 +0900

    로그인화면 완성

--- app/src/main/java/com/example/userinterface/MainActivity.java
+++ app/src/main/java/com/example/userinterface/MainActivity.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.userinterface.databinding.LoginBinding;
+import com.example.userinterface.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LoginBinding binding = LoginBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
     }
