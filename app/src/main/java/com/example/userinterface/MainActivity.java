4e1804c75c1b50ea3509db58554ec27beaa3f959
omj <alswo030203@gmail.com>
2024-11-06 20:22:28 +0900

    로그인화면 회원가입 화면 완성

--- app/src/main/java/com/example/userinterface/MainActivity.java
+++ app/src/main/java/com/example/userinterface/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.userinterface;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +14,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.userinterface.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        sign = binding.etSign;
+        //회원가입 클릭시 회원가입 창으로 이동
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, sign.class);
+                startActivity(intent);
+            }
+        });
     }
 }
