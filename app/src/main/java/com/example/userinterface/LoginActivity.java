129b27d6ff7e117efa680eb6c496561ac0a83049
ricky <aswmwhee0219@gmail.com>
2024-11-11 19:29:56 +0900

    firebase

--- app/src/main/java/com/example/userinterface/LoginActivity.java
+++ app/src/main/java/com/example/userinterface/LoginActivity.java
@@ -16,14 +16,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        //회원가입 클릭시 회원가입 창으로 이동
+        // 회원 가입 클릭시 회원가입 창으로 이동
         binding.tvSign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity (new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
+
+        // 로그인 클릭 시 메인 창으로 이동
+        binding.loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity (new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
     }
-
-
 }
