b81bbdd7282da208a6ba271367040584807efd37
Kim MinJae <kim1121403@gmail.com>
2024-11-16 21:14:51 +0900

    설정 창, 메뉴 화면

--- app/src/main/java/com/example/userinterface/MenuFragment.java
+++ app/src/main/java/com/example/userinterface/MenuFragment.java
@@ -2,6 +2,7 @@ package com.example.userinterface;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,5 +16,8 @@ public class MenuFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu, container, false);
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/userinterface/SettingActivity.java
@@ -0,0 +1,68 @@
+package com.example.userinterface;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.userinterface.databinding.ActivitySettingBinding;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySettingBinding binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // 뒤로 가기 버튼 클릭
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view == binding.backButton){
+                    finish();
+                }
+            }
+        });
+
+        // 알람 설정 버튼 클릭
+        binding.alramSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 닉네임 변경 클릭
+        binding.nicknameChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 비밀 번호 변경 클릭
+        binding.passwordChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 프로필 변경 클릭
+        binding.profileChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+    }
+}
